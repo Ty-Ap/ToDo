240299c4bd53067c4346c854636763b10b7a5d4f
Ty Aponte <aaustinsk8@aol.com>
2023-03-28 19:07:36 -0700

    have settings nav, and router-dom working, still refactoring and making settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
 
-import Todo from './Components/Todo';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Settings from './Context/Settings'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Todo from './Components/Todo'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <Todo />
-        <Footer />
-      </>
-    );
-  }
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      {/* Your home components and logic here */}
+    </div>
+  );
 }
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/settings" component={Settings} />
+        </Routes>
+      </div>
+      <Todo/>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
